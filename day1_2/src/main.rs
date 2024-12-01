03026b8196604d666ae931120fa131c6ba0ad21f
panicbit <panicbit.dev@gmail.com>
2024-12-01 23:45:13 +0100

    day 1 part 2

--- day1_2/src/main.rs
+++ day1_2/src/main.rs
@@ -1,6 +1,30 @@
+use std::collections::HashMap;
+
 use aoc::aoc;
 
 #[aoc(2024, 1, 2)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> i64 {
+    let mut left = Vec::new();
+    let mut right = HashMap::<i64, i64>::new();
+
+    for line in input.lines() {
+        let mut parts = line.split("   ");
+
+        let a: i64 = parts.next().unwrap().parse().unwrap();
+        let b: i64 = parts.next().unwrap().parse().unwrap();
+
+        left.push(a);
+        *right.entry(b).or_default() += 1;
+    }
+
+    let score: i64 = left
+        .into_iter()
+        .map(|a| {
+            let occurrences = right.get(&a).copied().unwrap_or_default();
+
+            a * occurrences
+        })
+        .sum();
+
+    score
 }
