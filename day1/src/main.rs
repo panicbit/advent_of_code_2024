4e3fe86601ba7b463149ce695c1268b4386c3595
panicbit <panicbit.dev@gmail.com>
2024-12-01 23:41:23 +0100

    day 1 part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,6 +1,28 @@
 use aoc::aoc;
 
 #[aoc(2024, 1, 1)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> i64 {
+    let mut left = Vec::new();
+    let mut right = Vec::new();
+
+    for line in input.lines() {
+        let mut parts = line.split("   ");
+
+        let a: i64 = parts.next().unwrap().parse().unwrap();
+        let b: i64 = parts.next().unwrap().parse().unwrap();
+
+        left.push(a);
+        right.push(b);
+    }
+
+    left.sort();
+    right.sort();
+
+    let sum: i64 = left
+        .into_iter()
+        .zip(right)
+        .map(|(a, b)| (a - b).abs())
+        .sum();
+
+    sum
 }
