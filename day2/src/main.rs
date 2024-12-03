4025a5a9f3f1321ef13b8d4fe035faeffa4a6560
panicbit <panicbit.dev@gmail.com>
2024-12-03 19:28:58 +0100

    advent of code day 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,36 @@
 use aoc::aoc;
+use itertools::Itertools;
 
 #[aoc(2024, 2, 1)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|line| {
+            line.split(" ")
+                .map(|level| level.parse::<i32>().unwrap())
+                .tuple_windows()
+                .map(|(a, b)| {
+                    if !(1..=3).contains(&(a - b).abs()) {
+                        return None;
+                    }
+
+                    if a < b {
+                        Some(Gradient::Ascending)
+                    } else {
+                        Some(Gradient::Descending)
+                    }
+                })
+                .tuple_windows()
+                .all(|(a, b)| match (a, b) {
+                    (Some(a), Some(b)) => a == b,
+                    (_, _) => false,
+                })
+        })
+        .count()
+}
+
+#[derive(PartialEq, Clone)]
+enum Gradient {
+    Ascending,
+    Descending,
 }
--- day2_2/src/main.rs
+++ day2_2/src/main.rs
@@ -1,6 +1,59 @@
 use aoc::aoc;
+use itertools::Itertools;
 
 #[aoc(2024, 2, 2)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> usize {
+    input
+        .lines()
+        .filter(|line| {
+            let levels = line
+                .split(" ")
+                .map(|level| level.parse::<i32>().unwrap())
+                .collect::<Vec<_>>();
+
+            if levels_safe(&levels) {
+                return true;
+            }
+
+            for (i, _) in levels.iter().enumerate() {
+                let mut levels = levels.clone();
+
+                levels.remove(i);
+
+                if levels_safe(&levels) {
+                    return true;
+                }
+            }
+
+            false
+        })
+        .count()
+}
+
+fn levels_safe(levels: &[i32]) -> bool {
+    levels
+        .iter()
+        .tuple_windows()
+        .map(|(a, b)| {
+            if !(1..=3).contains(&(a - b).abs()) {
+                return None;
+            }
+
+            if a < b {
+                Some(Gradient::Ascending)
+            } else {
+                Some(Gradient::Descending)
+            }
+        })
+        .tuple_windows()
+        .all(|(a, b)| match (a, b) {
+            (Some(a), Some(b)) => a == b,
+            (_, _) => false,
+        })
+}
+
+#[derive(PartialEq, Clone)]
+enum Gradient {
+    Ascending,
+    Descending,
 }
