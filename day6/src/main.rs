aff0feb979241ce9031269c33134ce10f13eede6
panicbit <panicbit.dev@gmail.com>
2024-12-06 13:08:50 +0100

    day 6

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,6 +1,64 @@
+use std::collections::HashMap;
+use std::iter;
+
 use aoc::aoc;
+use itertools::Itertools;
 
 #[aoc(2024, 6, 1)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> usize {
+    let grid = parse_grid(input);
+
+    guard_path(&grid).unique().count()
+}
+
+fn guard_path(grid: &Grid) -> impl Iterator<Item = Position> + '_ {
+    let mut directions = directions();
+    let (mut x, mut y) = find_start_position(grid);
+    let (mut xd, mut yd) = directions.next().unwrap();
+
+    iter::once((x, y)).chain(iter::from_fn(move || loop {
+        let next_position = (x + xd, y + yd);
+
+        let cell = grid.get(&next_position)?;
+
+        if *cell == '#' {
+            (xd, yd) = directions.next().unwrap();
+            continue;
+        }
+
+        (x, y) = next_position;
+
+        return Some(next_position);
+    }))
+}
+
+fn find_start_position(grid: &Grid) -> Position {
+    grid.iter()
+        .find(|(_, cell)| **cell == '^')
+        .map(|(position, _)| position)
+        .copied()
+        .unwrap()
+}
+
+type Grid = HashMap<(i32, i32), char>;
+type Position = (i32, i32);
+type Direction = (i32, i32);
+
+fn parse_grid(input: &str) -> Grid {
+    let mut grid = HashMap::new();
+
+    for (y, line) in input.lines().enumerate() {
+        for (x, ch) in line.chars().enumerate() {
+            let x = x as i32;
+            let y = y as i32;
+
+            grid.insert((x, y), ch);
+        }
+    }
+
+    grid
+}
+
+fn directions() -> impl Iterator<Item = Direction> {
+    [(0, -1), (1, 0), (0, 1), (-1, 0)].into_iter().cycle()
 }
--- day6_2/src/main.rs
+++ day6_2/src/main.rs
@@ -1,6 +1,91 @@
+use std::collections::{HashMap, HashSet};
+use std::iter;
+
 use aoc::aoc;
 
 #[aoc(2024, 6, 2)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> usize {
+    let grid = parse_grid(input);
+
+    looping_blockades(&grid).count()
+}
+
+fn looping_blockades(grid: &Grid) -> impl Iterator<Item = Position> + '_ {
+    grid.iter()
+        .filter(|&(position, cell)| {
+            if *cell != '.' {
+                return false;
+            }
+
+            let mut grid = grid.clone();
+
+            grid.insert(*position, '#');
+
+            path_contains_loop(guard_path(&grid))
+        })
+        .map(|(position, _)| *position)
+}
+
+fn path_contains_loop(path: impl Iterator<Item = (Position, Direction)>) -> bool {
+    let mut visited = HashSet::new();
+
+    for path_item in path {
+        if !visited.insert(path_item) {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn guard_path(grid: &Grid) -> impl Iterator<Item = (Position, Direction)> + '_ {
+    let mut directions = directions();
+    let (mut x, mut y) = find_start_position(grid);
+    let (mut xd, mut yd) = directions.next().unwrap();
+
+    iter::once(((x, y), (xd, yd))).chain(iter::from_fn(move || loop {
+        let next_position = (x + xd, y + yd);
+
+        let cell = grid.get(&next_position)?;
+
+        if *cell == '#' {
+            (xd, yd) = directions.next().unwrap();
+            continue;
+        }
+
+        (x, y) = next_position;
+
+        return Some((next_position, (xd, yd)));
+    }))
+}
+
+fn find_start_position(grid: &Grid) -> Position {
+    grid.iter()
+        .find(|(_, cell)| **cell == '^')
+        .map(|(position, _)| position)
+        .copied()
+        .unwrap()
+}
+
+type Grid = HashMap<(i32, i32), char>;
+type Position = (i32, i32);
+type Direction = (i32, i32);
+
+fn parse_grid(input: &str) -> Grid {
+    let mut grid = HashMap::new();
+
+    for (y, line) in input.lines().enumerate() {
+        for (x, ch) in line.chars().enumerate() {
+            let x = x as i32;
+            let y = y as i32;
+
+            grid.insert((x, y), ch);
+        }
+    }
+
+    grid
+}
+
+fn directions() -> impl Iterator<Item = Direction> {
+    [(0, -1), (1, 0), (0, 1), (-1, 0)].into_iter().cycle()
 }
