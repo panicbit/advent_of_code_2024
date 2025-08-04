d27f5866b15953506054f20e671457b2c74e962c
panicbit <panicbit.dev@gmail.com>
2025-08-05 01:43:25 +0200

    day 10 part 2

--- day10_2/src/main.rs
+++ day10_2/src/main.rs
@@ -1,6 +1,85 @@
+use std::collections::{BTreeMap, BTreeSet};
+
 use aoc::aoc;
 
 #[aoc(2024, 10, 2)]
-fn main(input: &str) -> i32 {
-    unimplemented!()
+fn main(input: &str) -> usize {
+    let mountain = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().filter_map(move |(x, ch)| {
+                ch.to_digit(10)
+                    .map(|elevation| ((x as isize, y as isize), elevation))
+            })
+        })
+        .collect::<Mountain>();
+
+    let result = trails(&mountain)
+        .map(|trail| paths(&mountain, trail).len())
+        .sum::<usize>();
+
+    result
+}
+
+type Mountain = BTreeMap<Pos, u32>;
+type Paths = BTreeSet<Vec<Pos>>;
+type Pos = (isize, isize);
+type Trail = Pos;
+
+fn trails(mountain: &Mountain) -> impl Iterator<Item = Pos> + use<'_> {
+    mountain
+        .iter()
+        .filter(|(_, elevation)| **elevation == 0)
+        .map(|(pos, _)| *pos)
+}
+
+fn paths(mountain: &Mountain, trail: Trail) -> Paths {
+    let mut paths = Paths::new();
+    let elevation = 0;
+
+    assert_eq!(mountain.get(&trail), Some(&elevation));
+
+    summits_rec(mountain, trail, elevation, &mut paths, &mut Vec::new());
+
+    paths
+}
+
+fn summits_rec(
+    mountain: &Mountain,
+    trail: Trail,
+    elevation: u32,
+    summits: &mut Paths,
+    path: &mut Vec<Pos>,
+) {
+    if elevation == 9 {
+        summits.insert(path.clone());
+        return;
+    }
+
+    for (pos, next_elevation) in neighbours(mountain, trail) {
+        if elevation + 1 != next_elevation {
+            continue;
+        }
+
+        path.push(pos);
+        summits_rec(mountain, pos, next_elevation, summits, path);
+        path.pop();
+    }
+}
+
+#[allow(clippy::identity_op)]
+fn neighbours(
+    mountain: &Mountain,
+    (x, y): (isize, isize),
+) -> impl Iterator<Item = ((isize, isize), u32)> + use<'_> {
+    [
+        (x - 1, y - 0),
+        (x + 0, y - 1),
+        (x + 0, y + 1),
+        (x + 1, y + 0),
+    ]
+    .into_iter()
+    .filter_map(|pos| mountain.get_key_value(&pos))
+    .map(|(pos, elevation)| (*pos, *elevation))
 }
