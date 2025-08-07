3e2dfbe7604101c253776444c2e4439d50c03ab4
panicbit <panicbit.dev@gmail.com>
2025-08-07 21:34:37 +0200

    refactor python day 12

--- day12_2/py/main.py
+++ day12_2/py/main.py
@@ -1,90 +1,136 @@
-from calendar import c
 from dataclasses import dataclass
 from enum import Enum, IntEnum, auto
 from operator import attrgetter, methodcaller
-from typing import Dict, Generator, List, Never, Set, Tuple
+from typing import Dict, Generator, List, Set, Tuple
 
 input = open("input.txt").read()
 
 type Plot = Tuple[int, int]
 type Label = str
-type Garden = Dict[Plot, Label]
 
 def main() -> None:
-    print(f"part 1: {part1(input)}")
-    print(f"part 2: {part2(input)}")
+    garden = Garden.parse(input)
+    regions = garden.regions()
 
-def part1(input: str) -> int:
-    garden = parse_input(input)
-    regions = find_regions(garden)
+    print(f"part 1: {part1(regions)}")
+    print(f"part 2: {part2(regions)}")
+
+def part1(regions: List["Region"]) -> int:
     total = 0
 
     for region in regions:
         area = len(region.plots)
-        circumference = len(region.perimeter())
-        price = area * circumference
+        perimeter = len(region.perimeter())
+        price = area * perimeter
         total += price
 
     return total
 
-def part2(input: str) -> int:
-    garden = parse_input(input)
-    regions = find_regions(garden)
+def part2(regions: List["Region"]) -> int:
     total = 0
 
     for region in regions:
         area = len(region.plots)
-        edges = region.edges()
-        price = area * len(edges)
+        sides = len(region.edges())
+        price = area * sides
         total += price
 
     return total
+
+@dataclass
+class Garden:
+    plot_labels: Dict[Plot, Label]
+    
+    @staticmethod
+    def parse(garden: str) -> "Garden":
+        plot_labels = {}
+
+        for y, line in enumerate(garden.splitlines()):
+            for x, label in enumerate(line):
+                plot_labels[(x, y)] = label
+
+        return Garden(plot_labels)
+    
+    def regions(self) -> List["Region"]:
+        remaining = dict(self.plot_labels)
+        regions = []
+
+        while remaining:
+            plot, label = next(iter(remaining.items()))
+            region = self.region(plot, label)
+
+            regions.append(region)
+
+            for plot in region.plots:
+                remaining.pop(plot)
+            
+        
+        return regions
+    
+    def region(self, plot: Plot, label: Label) -> "Region":
+        plots: Set[Plot] = set()
+
+        self.region_rec(plot, label, plots)
+
+        return Region(label, plots)
+
+    def region_rec(self, plot: Plot, label: Label, plots: Set[Plot]) -> None:
+        if plot in plots:
+            return
+
+        if self.plot_labels.get(plot) != label:
+            return
+
+        plots.add(plot)
+
+        for neighbour in neighbours(plot): 
+            self.region_rec(neighbour, label, plots)
 
 @dataclass
 class Region:
     label: str
     plots: Set[Plot]
 
-    def perimeter(self) -> List["Side"]:
-        sides = []
+    def perimeter(self) -> List["Edge"]:
+        edges = []
 
         for plot in self.plots:
             for neighbour, dir in neighbours_with_dirs(plot):
                 if neighbour not in self.plots:
-                    sides.append(Side(dir, plot))
+                    edges.append(Edge(dir, plot))
 
-        return sides
+        return edges
     
-    def edges(self) -> List[List["Side"]]:
-        def inner() -> Generator[List[Side]]:
-            sides = self.perimeter()
-            sides = sorted(sides, key = methodcaller('axis_coord'))
-            sides = sorted(sides, key = methodcaller('cross_axis_coord'))
-            sides = sorted(sides, key = attrgetter('dir'))
+    def edges(self) -> List[List["Edge"]]:
+        def inner() -> Generator[List[Edge]]:
+            edges = self.perimeter()
+            edges = sorted(edges, key = methodcaller('axis_coord'))
+            edges = sorted(edges, key = methodcaller('cross_axis_coord'))
+            edges = sorted(edges, key = attrgetter('dir'))
 
-            edge: List[Side] = []
+            side: List[Edge] = []
 
-            for side in sides:
-                if not edge:
-                    edge.append(side)
+            for edge in edges:
+                if not side:
+                    side.append(edge)
                     continue
 
-                last = edge[-1];
-                dirs_match = last.dir == side.dir
-                cross_axes_match = last.cross_axis_coord() == side.cross_axis_coord()
-                axes_consecutive = last.axis_coord() + 1 == side.axis_coord()
+                last = side[-1];
+                dirs_match = last.dir == edge.dir
+                cross_axes_match = last.cross_axis_coord() == edge.cross_axis_coord()
+                axes_consecutive = last.axis_coord() + 1 == edge.axis_coord()
 
                 if all([dirs_match, cross_axes_match, axes_consecutive]):
-                    edge.append(side)
+                    side.append(edge)
                 else:
-                    yield edge
-                    edge = [side]
+                    yield side
+                    side = [edge]
 
-            yield edge
+            yield side
         return list(inner())
 
 @dataclass
-class Side:
+class Edge:
     dir: "Dir"
     plot: Plot
 
@@ -121,59 +167,8 @@ class Axis(Enum):
             case Axis.Horizontal: return y
             case Axis.Vertical: return x
 
-def parse_input(input: str) -> Garden:
-    garden = {}
-
-    for y, line in enumerate(input.splitlines()):
-        for x, label in enumerate(line):
-            garden[(x, y)] = label
-
-    return garden
-
-def find_regions(garden: Garden) -> List[Region]:
-    remaining = dict(garden)
-    regions = []
-
-    while remaining:
-        plot, label = next(iter(remaining.items()))
-        region = find_region(plot, label, garden)
-
-        regions.append(region)
-
-        for plot in region.plots:
-            remaining.pop(plot)
-        
-    
-    return regions
-
-def find_region(plot: Plot, label: Label, garden: Garden) -> Region:
-    plots: Set[Plot] = set()
-
-    find_region_rec(plot, label, garden, plots)
-
-    return Region(label, plots)
-
-def find_region_rec(plot: Plot, label: Label, garden: Garden, plots: Set[Plot]) -> None:
-    if plot in plots:
-        return
-
-    if garden.get(plot) != label:
-        return
-
-    plots.add(plot)
-
-    for neighbour in neighbours(plot): 
-        find_region_rec(neighbour, label, garden, plots)
-
 def neighbours(plot: Plot) -> List[Plot]:
-    x, y = plot
-
-    return [
-        (x, y -1),
-        (x, y + 1),
-        (x - 1, y),
-        (x + 1, y),
-    ]
+    return list(neighbour for neighbour, _ in neighbours_with_dirs(plot))
 
 def neighbours_with_dirs(plot: Plot) -> List[Tuple[Plot, Dir]]:
     x, y = plot
